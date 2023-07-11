cf8f8f91606a7d4dff87efe70ba9867a2ddb92f0
Sahil-Rathod <sahilrathod2810@gmail.com>
2023-07-11 18:24:11 +0530

    Testing

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Image, View, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import IndexPage from "./pages/IndexPage";
+import DetailPage from "./pages/DetailPage";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Index"
+          component={IndexPage}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="DetailPage"
+          component={DetailPage}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 50,
+  },
+  image: {
+    width: 200,
+    height: 200,
+    resizeMode: "contain",
+  },
+  input: {
+    width: 300,
+    margin: 12,
+    paddingBottom: 3,
+    borderBottomWidth: 1,
   },
 });
